00b9618908bef15c4e74a18e9c8704c1e5c08105
Saleh545 <141879902+Saleh545@users.noreply.github.com>
2023-11-16 00:13:39 -0800

    Doctonline bitdi

--- Js/home.js
+++ Js/home.js
@@ -1,23 +1,24 @@
 $('.popular-slider').owlCarousel({
-    loop:true,
-    margin:28,
-   smartSpeed:1500,
-   autoWidth:false,
-   dots:true,
-   nav:true,
+    loop: true,
+    margin: 28,
+    smartSpeed: 1500,
+    autoWidth: false,
+    dots: false,
+    nav: false,
     
-    responsive:{
-        0:{
-            items:1
+    responsive: {
+        0: {
+            items: 1
         },
-        600:{
-            items:1
+        600: {
+            items: 2
         },
-        1200:{
-            items:1
+        1200: {
+            items: 3
         }
     }
-})
+});
+
 
 
 
